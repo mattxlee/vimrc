c49bddd8a7a46d831cda679330abb94adddabcba
Matthew Lee <mattxlee@gmail.com>
2022-09-22 11:34:21 +0800

    Remove neovim support.

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ ! -x "$(command -v python3)" ]; then
     echo "Please install python3-dev!"
 fi
 
-VIMRC_PATH=$HOME/.config/nvim
+VIMRC_PATH=$HOME/vimrc
 if [ ! -d $VIMRC_PATH ]; then
     git clone --depth 1 https://github.com/mattxlee/vimrc $VIMRC_PATH
 fi
@@ -50,7 +50,7 @@ instplugins() {
 echo "Installing plugins..."
 if [ -x "$(command -v vim)" ]; then
     echo " vim - generate .vimrc"
-    echo 'source ~/.config/nvim/init.vim' > $HOME/.vimrc
+    echo 'source ~/vimrc/init.vim' > $HOME/.vimrc
     echo " vim - plugin manager"
     $(curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1)
@@ -58,14 +58,6 @@ if [ -x "$(command -v vim)" ]; then
     instplugins vim
     echo " vim - done"
 fi
-if [ -x "$(command -v nvim)" ]; then
-    echo " neovim - plugin manager"
-    $(curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1)
-    echo " neovim - plugins"
-    instplugins nvim
-    echo " neovim - done"
-fi
 
 echo "Run :LeaderfInstallCExtension to speed up LeaderF"
-echo "You might need to install tree-sitter-cli, see more info: http://tree-sitter.github.io/tree-sitter/creating-parsers#installation"
+echo "\n"
