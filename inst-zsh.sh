fd19bc5f06531420b8f988967db0ef1b96c13c17
Matthew Lee <mattxlee@gmail.com>
2022-10-15 23:26:06 +0800

    Improve zsh installation script.

--- inst-zsh.sh
+++ inst-zsh.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-sudo apt install fzf --yes
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cp .zshrc $HOME
+if [ ! -x $(command -v fzf) ]; then
+    echo "command `fzf` cannot be found, you need to install it."
+fi
+
+clone_plugin() {
+    SOURCE_URL=$1
+    DEST_DIR=$2
+    if [ ! -d $DEST_DIR ]; then
+        git clone $SOURCE_URL $DEST_DIR
+    fi
+}
+
+clone_plugin https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+clone_plugin https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+if [ ! -f $HOME/.zshrc ]; then
+    cp .zshrc $HOME/.zshrc
+fi
 
